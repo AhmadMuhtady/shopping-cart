0bacfdb3ef9f5317ec9d0b8b14f6884df406b5c1
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-10-29 21:26:11 +0200

    updated useProducts file to fetch data

---
+++ src/hooks/useProducts.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+export const useProducts = () => {
+	const [products, setProducts] = useState([]);
+	const [categories, setCategories] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			setError(null);
+			try {
+				const [productsRes, categoriesRes] = await Promise.all([
+					fetch('https://fakestoreapi.com/products'),
+					fetch('https://fakestoreapi.com/products/categories'),
+				]);
+
+				if (!productsRes.ok || !categoriesRes.ok)
+					throw new Error('Failed to fetch Data!');
+				const promiseData = await productsRes.json();
+				const categoriesData = await categoriesRes.json();
+
+				console.log(promiseData);
+				console.log(categoriesData);
+
+				setProducts(promiseData);
+				setCategories(categoriesData);
+			} catch (err) {
+				setError(err.message);
+				setLoading(false);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchProducts();
+	}, []);
+
+	return { products, categories, loading, error };
+};
